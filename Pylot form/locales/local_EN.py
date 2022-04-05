3b37d850751caff9ddf58413c00a74f662f50804
MAZASA.mzs <sasha_school_2005@mail.ru>
2022-04-06 00:31:34 +0300

    Added RU and EN locales

---
+++ Pylot form/locales/local_EN.py
@@ -0,0 +1,82 @@
+#EN local for Pylot form
+
+cmds_dict = {
+	# Drone commands
+    "command": "command",
+    "takeoff": "takeoff",
+    "land": "land",
+    "streamon": "streamon",
+    "streamoff": "streamoff",
+    "emergency": "emergency",
+    "up": "up",
+    "down": "down",
+    "left": "left",
+    "right": "right",
+    "forward": "forward",
+    "назад": "back",
+    "cw": "cw",
+    "ccw": "ccw",
+    "flip": "flip",
+    "go": "go",
+    "curve": "curve",
+    "speed": "speed",
+    # "Frontend" (or form) commands 
+    "snapshot": "snapshot",
+    "scanning": "scanning",
+    "sleep": "sleep",
+    # Cyclies
+    "repeat": "repeat",
+    "done": "done",
+}
+
+tello_params_dict = {
+    "pitch": "Тангаж",
+    "roll": "Крен",
+    "yaw": "Рысканье",
+    "vgx": "Скрость X",
+    "vgy": "Скрость Y",
+    "vgz": "Скрость Z",
+    "templ": "Температура мин",
+    "temph": "Температура макс",
+    "tof": "Высота (см)",
+    "h": "Вертикальное ускорение",
+    "bat": "Батарея",
+    "baro": "Барометр",
+    "time": "Время",
+    "agx": "Ускорение X",
+    "agy": "Ускорение Y",
+    "agz": "Ускорение Z",
+}
+
+
+end_of_init = "Конец инициализации"
+
+took_photo_ok = "Сделал фото"
+took_photo_not_ok = "Не смог сделать фото"
+skanning_stop = "Остановил сканирование"
+skanning_start = "Начал сканирование с частотой (сек):"
+
+clean_log = "Очистить лог"
+
+open_file = "Открыть"
+save_file = "Сохранить"
+
+run_code_break = "Прервать"
+run_code_start = "Запустить"
+
+run_code_sleep = "Сплю секунд:"
+run_code_begin = "Начинаю выполнение кода"
+run_code_command = "Выполняю команду:"
+run_code_response = "Статус завершения:"
+run_code_finished = "Завершил выполнение кода"
+
+disconnect_dron = "Отключить"
+connect_to_dron = "Подключить"
+
+tello_connected = "Tello подключён"
+tello_disconnected = "Tello отключён"
+
+cant_connect_drone = "Не могу подключиться к дрону"
+
+norun_syntax_error = "Не могу начать выполнение, ошибка: "
+norun_not_connected = "Не могу начать выполнение, дрон не подключён"
